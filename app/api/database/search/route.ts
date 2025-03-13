95a4f7add8381907fb3b5a84151615cc72179f52
federicop1990 <federico.patruno@gmail.com>
2025-03-13 11:40:15 +0100

    Added revalidation to fix stale data after database search

--- app/api/database/search/route.ts
+++ app/api/database/search/route.ts
@@ -1,6 +1,8 @@
 import { getDatabaseResults } from "@/lib/discogs/actions";
 import { NextRequest, NextResponse } from "next/server";
 
+export const revalidate = 10;
+
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request?.url);
   const results = await getDatabaseResults(searchParams);
